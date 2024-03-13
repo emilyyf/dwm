6e2079cbae9f0c6eddb8bc8641ac8c3172d62d88
Emily <68402015+emilyyf@users.noreply.github.com>
2024-03-13 19:08:43 -0300

    feat: disable mouse accel and screensaver

--- config.def.h
+++ config.def.h
@@ -27,6 +27,9 @@ static const char *colors[][3]      = {
 static const char *const autostart[] = {
 	"sh", "-c", "/home/emily/.fehbg", NULL,
 	"setxkbmap", "-option", "compose:ralt", NULL,
+	"xset", "s", "off", NULL,
+	"xset", "-dpms", NULL,
+	"xinput", "set-prop", "8", "libinput Accel Profile Enabled", "0", "1", "0", NULL,
 	"sh", "-c", "while :; do xsetroot -name \"$(date +'%a %b %d %H:%M %Y')\"; sleep 60; done", NULL,
 	NULL /* terminate */
 };
