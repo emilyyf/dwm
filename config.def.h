70ba291722f03b173c6ca18efa60369b2bceb215
Emily <68402015+emilyyf@users.noreply.github.com>
2024-03-21 09:33:29 -0300

    feat: set email clients and carla rules

--- config.def.h
+++ config.def.h
@@ -44,9 +44,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class               instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",              NULL,       NULL,       0,            1,           -1 },
+	{ "thunderbird",       NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "tutanota-desktop",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Carla2",            NULL,       NULL,       1 << 7,       0,           -1 },
 };
 
 /* layout(s) */
