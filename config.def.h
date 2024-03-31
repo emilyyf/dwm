dab13e3698a8c0f4fec52a6bb0fb62409b70150d
Emily <68402015+emilyyf@users.noreply.github.com>
2024-03-31 13:24:12 -0300

    feat: set dmenu options

--- config.def.h
+++ config.def.h
@@ -77,7 +77,19 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon,
+	"-fn", dmenufont,
+	"-nb", col_gray1,
+	"-nf", col_gray3,
+	"-sb", col_cyan,
+	"-sf", col_gray4,
+	"-nhb", col_gray1,
+	"-nhf", col_gray3,
+	"-shb", col_cyan,
+	"-shf", col_gray4,
+	"-l", "6",
+	"-c",
+	NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
