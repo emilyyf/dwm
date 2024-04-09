41a35a4dda4219959164f14f095f1d7b5799b956
Emily <emily@anaboth.com>
2024-04-09 08:52:02 -0300

    feat: change theme to catppuccin

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -13,11 +13,12 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#770066";
+static const char col_gray1[]       = "#24273a";
+static const char col_gray2[]       = "#b7bdf8";
+static const char col_gray3[]       = "#b8c0e0";
+static const char col_gray4[]       = "#cad3f5";
+static const char col_cyan[]        = "#f5bde6";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -78,17 +79,6 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon,
-	"-fn", dmenufont,
-	"-nb", col_gray1,
-	"-nf", col_gray3,
-	"-sb", col_cyan,
-	"-sf", col_gray4,
-	"-nhb", col_gray1,
-	"-nhf", col_gray3,
-	"-shb", col_cyan,
-	"-shf", col_gray4,
-	"-l", "6",
-	"-c",
 	"-i",
 	NULL };
 static const char *termcmd[]  = { "st", NULL };
