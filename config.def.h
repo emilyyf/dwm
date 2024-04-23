4e4ecf229b7a314e3d0ebd474fbf9300f00b6bb0
emilyyf <emily@anaboth.com>
2024-04-23 14:13:13 -0300

    feat: change screenshot command

--- config.def.h
+++ config.def.h
@@ -87,12 +87,13 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon,
 	NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *spectaclecmd[] = { "spectacle", "-r", NULL };
+static const char *scrotcmd[] = { "scrot", "-s", "-f", "$HOME/Pictures/Screenshots/%Y-%m-%d_%H.%M.%S.png", "-e", "xclip -selection clipboard -t image/png -i $f", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = spectaclecmd } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = scrotcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
